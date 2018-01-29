d14be77f65375bed5e47dd9cc73c0e317785971f
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2018-01-29 21:19:12 +0200

    grouped info snps data

--- generate-genes.R
+++ generate-genes.R
@@ -1,20 +1,51 @@
-# this file generates genes related to snps list
+# Target : build a mapping of SNPs to pathways 
+#  - by first looking for closest genes in the vicinity of a SNP, 
+#  - or via eQTL mapping (expression quantitative trait loci)
 # ``````````````````````````````````````````````````
 # First : lets simulate SNPs data
 #---------------------------------
 snps.sourcePath = "generate-snps-data.R"
 source(snps.sourcePath)
 
-# number of target snps
-n.snps <- 150
+# number of target snps 
+# "NB : Must be divisable by 4"
+n.snps <- 120
 
 # get pathogenecity matrix
 snps.pathogenicity <- get.Snps.matrix(n.snps)
 #colnames(snps.pathogenicity)
   
 # get snps names
-snps.names <- get.Snps.list(n.snps)
+snps.info <- as.data.frame( get.Snps.list(n.snps))
 #head(snps.names)
   
 # update matric snps names
-colnames(snps.pathogenicity) = snps.names
+colnames(snps.pathogenicity) = snps.info$snpname
+# ````````````````````````````````````````````````````
+# Secound : generate genes from current snps
+#--------------------------------------------
+# map snps to genes 
+# http://jef.works/blog/2016/12/06/mapping-snps-and-peaks-to-genes-in-R/
+
+# Try documentation example 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
